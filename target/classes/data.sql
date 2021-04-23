c33a425749e5b776170b3227d41daab12941c56d
Surapong <surapong.bird@mail.kmutt.ac.th>
2021-04-24 00:35:31 +0700

    rebase dev2

---
+++ target/classes/data.sql
@@ -0,0 +1,16 @@
+
+
+insert into colors(colorName) values('red');
+insert into colors(colorName) values('green');
+insert into colors(colorName) values('blue');
+
+insert into brands(brandName) values('TOA');
+insert into brands(brandName) values('FOURSEASON');
+insert into brands(brandName) values('begershield');
+
+insert into products(date,colors_Id,brands_id) 
+values('2001-02-22',1,1);
+insert into products(date,colors_Id,brands_id) 
+values('1999-04-9',2,2);
+insert into products(date,colors_Id,brands_id) 
+values('2000-08-16',3,3);
