1a2ae1dc95621c7ccbc94570e10e6d1e76f82f58
Meawmfia <boon20002543@hotmail.com>
2021-04-26 13:50:23 +0700

    [UPDATE] crossOrigin * : config CORS policy

--- src/main/java/com/springboot/app/controller/ColorController.java
+++ src/main/java/com/springboot/app/controller/ColorController.java
@@ -3,6 +3,7 @@ package com.springboot.app.controller;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,8 @@ import com.springboot.app.repository.ColorRepository;
 
 @RestController
 public class ColorController {
-    
+	@CrossOrigin(origins = "*")
+
 	@GetMapping("/color")
 	public Collection<Color> color(){
 		return this.colorRepository.findAll();
--- src/main/java/com/springboot/app/controller/ProductController.java
+++ src/main/java/com/springboot/app/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.springboot.app.controller;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import com.springboot.app.repository.ProductRepository;
 
 @RestController
 public class ProductController {
+	@CrossOrigin(origins = "*")
 
 	@GetMapping("/product")
 	public Collection<Product> product() {
