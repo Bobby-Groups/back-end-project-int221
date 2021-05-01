d65e4003de6b5666abbadec4cd0cff109f0299e9
Surapong <surapong.bird@mail.kmutt.ac.th>
2021-05-01 18:56:37 +0700

    [update] clear unuse file

--- src/main/java/com/springboot/app/SpringbootbackendApplication.java
+++ src/main/java/com/springboot/app/SpringbootbackendApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-import com.springboot.app.service.StorageProperty;
 
 @SpringBootApplication
-@EnableConfigurationProperties({StorageProperty.class})
+
 public class SpringbootbackendApplication {
 
 	public static void main(String[] args) {
